75c04fbb2d014da11f9cb8978d0c593d85bbc4ff
mariana4ads <mariana4ads@gmail.com>
2025-07-15 00:12:02 -0300

    resposta do exercicio 3 desafio c

---
+++ exercicio-3/desafio-3c.js
@@ -0,0 +1,8 @@
+// c) Arredonde o resultado de 9.7 dividido por 2 para o inteiro mais próximo
+
+const number = 9.7;
+const divider = 2;
+
+const nextInt = Math.round(number/divider)
+
+console.log("O numero inteiro mais proximo de 9.7/2 é: " + nextInt);
