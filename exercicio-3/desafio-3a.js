c185eea3622f8296ed51a617864d0e77f24e0b33
mariana4ads <mariana4ads@gmail.com>
2025-07-15 00:05:59 -0300

    resposta do exercicio 3 desafio a

--- exercicio-3/desafio-3a.js
+++ exercicio-3/desafio-3a.js
@@ -1,5 +1,7 @@
+// a) Calcule a média de 3 números: 15, 25 e 38
+
 const a = 15;
 const b = 25;
 const c = 38;
 
-console.log("A média dos números a, b e c é :" + ( a + b + c)/ 3);
+console.log("A média dos números 15, 25 e 38 é: " + ( a + b + c) / 3);
