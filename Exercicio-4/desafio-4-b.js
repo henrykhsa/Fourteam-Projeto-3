c517880f7ca3109f6153fba579b640e74fa40829
mariana4ads <mariana4ads@gmail.com>
2025-07-20 22:55:38 -0300

    Resposta do exercicio 4 desafio ba

---
+++ Exercicio-4/desafio-4-b.js
@@ -0,0 +1,10 @@
+// b) Verifique se um número é par ou ímpar
+
+const number = 9; 
+
+if (number % 2 === 0) {
+
+  console.log("O número " + number + " é par.");
+} else {
+  console.log("O número " + number + " é impar");
+}
