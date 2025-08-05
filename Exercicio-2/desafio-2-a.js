79902b3c0dd062e0f25224963ed7e16bc7a77865
mariana4ads <mariana4ads@gmail.com>
2025-08-05 20:22:34 -0300

    respostas do exercicio 2

---
+++ Exercicio-2/desafio-2-a.js
@@ -0,0 +1,7 @@
+// a) Extraia o primeiro nome usando o método slice ou substring
+
+const firstName = "Maria";
+const lastName = "Silva";
+const fullName = firstName + " " + lastName;
+
+console.log("Primeiro nome extraído:", fullName.slice(0, firstName.length));
